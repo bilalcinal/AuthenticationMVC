dab038a741b261b3b5b26d016ca916a181a05f78
Bilal cinal <73084109+bilalcinal@users.noreply.github.com>
2023-09-11 18:35:35 +0300

    bugfix

--- MyProject/Program.cs
+++ MyProject/Program.cs
@@ -54,7 +54,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Authentication}/{action=Login}/{id?}");
 
 IConfiguration _configuration = new ConfigurationBuilder()
                             .AddJsonFile("appsettings.json")
